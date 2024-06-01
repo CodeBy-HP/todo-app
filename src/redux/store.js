380ded4f8afeff19b9888c888ef38588a9327adf
Harsh Patel <145273554+CodeBy-HP@users.noreply.github.com>
2024-06-01 10:14:28 +0530

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
 import { createStore } from "redux";
 import todoReducer from "./reducers";
-import { composeWithDevTools } from '@redux-devtools/extension';// Importing composeWithDevTools from redux-devtools-extension
+
 
 // Creating the Redux store with the todoReducer and DevTools integration
-const store = createStore(todoReducer, composeWithDevTools());
+const store = createStore(todoReducer);
 
 export default store; // Exporting the created Redux store
